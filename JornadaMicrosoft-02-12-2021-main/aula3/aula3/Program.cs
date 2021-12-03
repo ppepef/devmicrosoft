7652745c87663617fcf0db54ad290bd99e6d8854
ppepef <ppfranca@live.com>
2021-12-02 21:05:25 -0300

    Atualizando as aulas

---
+++ JornadaMicrosoft-02-12-2021-main/aula3/aula3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace aula3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   condicaoIf();
+            //escolha();
+            //enquanto();
+            para();
+        }
+
+        static public void para()
+        {
+            int numero;
+            for (numero = 1; numero <= 100; numero++)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+
+        static public void enquanto()
+        {
+            Console.WriteLine("Digite um numero:");
+            int numero = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            while (numero != 0)
+            {
+                soma += soma;
+                numero = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(soma);
+        }
+
+        static public void escolha()
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            switch (hora)
+            {
+                case < 12: Console.WriteLine("Bom dia!"); break;
+                case < 18: Console.WriteLine("Boa tarde!"); break;
+                default: Console.WriteLine("Boa noite!"); break;
+
+            }
+        }
+
+        static public void condicaoIf()
+        {
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+
+            if (hora >= 12 && hora < 18)
+            {
+                Console.WriteLine("Boa tarde");
+            }
+
+            if (hora >= 18)
+            {
+                Console.WriteLine("Boa noite!");
+            }
+
+        }
+
+
+    }
+}
---
+++ Professor_Lucas/aula_02/Aula02/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args) //classe pricipal;
+        {
+            //Para executar basta retirar o comentario da função desejada.
+
+            //POR EXEMPLO: abaixo vai ser executado apenas OperadorRelacional.
+            
+
+            //Inicio();
+            //OperadorAritmetico();
+            //OperadorAtribucao();
+            OperadorRelacional();
+            // OperadorLogico();
+            //Entrada();
+            //Saida();
+
+        }
+
+
+        public static void Inicio()
+        {
+
+            //COMO DECLARAR UMA VARIAVEL.
+            // TIPO depois o NOME ; <- no final de todas as linhas.
+
+            //linha verde = a variavel criada não esta sendo utilizada.
+
+            //linha vermelha = esta falando algo, vai dar erro de compilação.
+
+            // 1 sinal de igual representa atribuição.
+
+            string nome = "Lucas";
+            int idade = 28;
+
+            //Console.WriteLine(nome);
+            //Console.WriteLine(idade);
+            Console.WriteLine(nome + " " + idade);
+
+
+        }
+
+        public static void OperadorAritmetico()
+        {
+
+            int primeiro = 1;
+            int segundo = 10;
+            int terceiro = 20;
+
+            Console.WriteLine("Soma de todos os números: " + (primeiro + segundo + terceiro));
+            Console.WriteLine("Subtração: " + (terceiro - segundo - primeiro));
+            Console.WriteLine("Multiplicação: " + (terceiro * segundo * primeiro));
+            Console.WriteLine("Divisão: " + (terceiro / segundo));
+        }
+
+        public static void OperadorAtribucao()
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine("------------");
+
+            a += 2;
+            Console.WriteLine(a);
+            Console.WriteLine("------------");
+
+
+            string alfabeto = "ABC";
+            alfabeto += "DEF";
+            Console.WriteLine(alfabeto);
+
+            Console.WriteLine("------------");
+
+            a = 10; //receber 10.
+            a *= 3; //10 * 3;
+            Console.WriteLine(a);
+
+        }
+
+        public static void OperadorRelacional()
+        {
+
+
+            int a = 10;
+            bool resposta = a >= 10; //resposta sempre será TRUE OU FALSE.
+
+            Console.WriteLine("Valor da resposta:" + resposta);
+
+
+            bool menor = a < 10;
+            bool maiorIgual = a >= 30;
+            bool igual = (a == 10);  //igualdade
+            bool diferente = (a != 10); //diferente.
+
+
+            Console.WriteLine("Valor da resposta menor :" + menor);
+            Console.WriteLine("Valor da resposta maiorIgual :" + maiorIgual);
+            Console.WriteLine("Valor da resposta igual :" + igual);
+            Console.WriteLine("Valor da resposta diferente :" + diferente);
+
+        }
+
+        public static void OperadorLogico()
+        {
+
+
+            int salario1 = 100;
+            int salario2 = 50;
+            int salario3 = 200;
+
+            //Quero saber se o salario1 é maior que o 2 e tbm maior que o 3.
+
+            //&& significa E
+            bool resultadoSalario = salario1 > salario2 && salario1 > salario3;
+
+            // primeira condicao = (TRUE OU FALSE) E ( TRUE OU FALSE)
+
+            //Console.WriteLine(resultadoSalario);
+
+
+            //Quero saber se o salario 2 menor que o salario 1 ou maior que salario 3
+            bool resultado2 = salario2 < salario1 || salario2 > salario3;
+            //TRUE                 //FALSE
+
+
+            //Console.WriteLine(resultado2);
+
+
+            //Quero saber se o salario2  é maior que o salario 3
+            bool resultado3 = !(salario2 > salario3);
+            //! quer saber do contrario.
+
+            //! = quero saber se é falso.
+
+
+            Console.WriteLine(resultado3);
+
+
+        }
+
+
+        public static void Entrada()
+        {
+            Console.WriteLine("Entrada de dados");
+            Console.WriteLine("Digite o seu nome");
+
+            string nomeDoUsuario = Console.ReadLine();
+
+            Console.WriteLine("O nome digitado é: " + nomeDoUsuario);
+
+
+        }
+
+        public static void Saida()
+        {
+
+            int idade = 28;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            //Console.WriteLine("Maria tem 28 anos e tem saldo igual a 10.35784 reais.");
+
+            //interpolação. { } representar a variavel.
+
+            //o primeiro {} sempre inicia do zero.
+            // o segundo {} vai na sequencia. 1 ,....
+
+            //Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais.", nome, idade, saldo);
+
+            Console.WriteLine(saldo);
+            Console.WriteLine(saldo.ToString("F2"));
+
+            Console.WriteLine(saldo.ToString("F4"));
+
+
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+
+        public static void Parse()
+        {
+
+            //string - toString().
+
+            //parse
+
+
+            double numero = 10.45;
+            string numero2 = "20";
+
+            //Parse
+
+            Console.WriteLine(double.Parse(numero2) + numero);
+
+
+
+        }
+
+        public static void Exercicio()
+        {
+            /* Execicio 1  */
+
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            int idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            string forma = @"
+
+
+                        Usando os valores das variáveis, produza a seguinte saída na tela do console:
+
+                        Produtos:
+                        Computador, cujo preço é $ 2100,00
+                        Mesa de escritório, cujo preço é $ 650,20
+
+                        Registro: 30 anos de idade, código 5290 e gênero M
+
+
+                        Medida com oito casas decimais: 53,23456700
+                        Arredondado (três casas decimais): 53,235
+                        Separador decimal invariant culture: 53.235
+
+
+
+                     ";
+
+
+            //EXERCICIO 2 
+            //Elabore um algoritmo que receba e calcule a idade média de 5 alunos.
+
+        }
+
+
+    }
+}
